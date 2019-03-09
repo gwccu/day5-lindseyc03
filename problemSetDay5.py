99148b8b75f31dfb3c6127a54fc497647c3c39b8
lindseyc03 <lindsey.cr123@gmail.com>
2019-03-09 17:27:05 +0000

    worked on problem set 5

--- problemSetDay5.py
+++ problemSetDay5.py
@@ -1,1 +1,35 @@
 # File name: problemSetDay5.py
+for a in range(5):
+    print('a.', a)
+
+for b in range(5,8):
+    print ('b.', b)
+for c in range (5,20,3):
+    print ("c.", c )
+for d in range (0,35,5):
+    print('d.', d)
+a = 0
+while a<5:
+    print(a)
+    a+=1
+b = 5
+while b<8:
+    print(b)
+    b += 1
+c = 5
+while c<18:
+    print(c)
+    c += 3
+d = 0
+while d<35:
+    print(d)
+    d += 5
+
+for i in range(1):
+   
+
+
+
+
+
+
