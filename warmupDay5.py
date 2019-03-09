f0620f8751fcce6dcbb9c3dd1234f570c6c4b267
lindseyc03 <lindsey.cr123@gmail.com>
2019-03-09 15:29:32 +0000

    worked on warmup

--- warmupDay5.py
+++ warmupDay5.py
@@ -1,1 +1,9 @@
 # File name: warmupDay5.py
+
+myName = "Lindsey"
+guess = input("Guess my name ")
+
+while guess != myName:
+    guess = input("That's not my name, try again ")
+
+print('Good job, that is my name')
